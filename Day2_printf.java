ea009e1bdd52cc2c50de13095b73fea4397a477c
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-09-03 17:51:22 +0800

    Day2_printf.java

---
+++ Day2_printf.java
@@ -0,0 +1,21 @@
+package day2;
+
+public class Day2 {
+
+    public static void main(String[] args) {
+        // Deklarasi Variabel
+        String Asal = "Papalang";
+        int Umur = 19;
+        String Nama = "Yulia sagita putri";
+
+        // Mencetak Menggunakan Printf
+        System.out.printf(" Nama: %s \n ", Nama);
+        System.out.printf("Asal: %s \n ", Asal);
+        System.out.printf("Umur: %d Tahun \n ", Umur);
+
+        // Menggabungkan format dalam satu baris
+        System.out.printf("Hai, Nama saya %s, saya berusia %d Tahun dan saya berasal dari %s \n ", Nama, Umur, Asal);
+
+    }
+
+}
