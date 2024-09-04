685c62999d1346b53d6590a068316fd004b1d33f
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-09-04 16:07:01 +0800

    KombinasiPrintf_danPrint_lain.java

---
+++ Day3_KombinasiPrintf_danPrint_lain.java
@@ -0,0 +1,21 @@
+package days3;
+
+public class Days3 {
+
+    public static void main(String[] args) {
+
+        System.out.println("+-------------+--------+");
+        System.out.println("| Nama Barang | Jumlah |");
+        System.out.println("+-------------+--------+");
+
+        System.out.printf("| %-11s | %-6s |\n", "sabun", "2");
+        System.out.printf("| %-11s | %-6s |\n", "shampoh", "10");
+        System.out.printf("| %-11s | %-6s |\n", "sikat gigi", "2");
+        System.out.printf("| %-11s | %-6s |\n", "pasta gigi", "1");
+        System.out.printf("| %-11s | %-6s |\n", "sabun cuci", "2");
+        System.out.printf("| %-11s | %-6s |\n", "molto", "5");
+        System.out.printf("| %-11s | %-6s |\n", "snack", "5");
+        System.out.print("+-------------+--------+ \n");
+    }
+
+}
