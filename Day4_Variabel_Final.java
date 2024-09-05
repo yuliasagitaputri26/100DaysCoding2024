661260c05c9c6a546e88b1b9c2475e8360d6fa16
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-09-05 18:33:26 +0800

    Day4_Variabel_Final.java

---
+++ Day4_Variabel_Final.java
@@ -0,0 +1,16 @@
+package day4;
+
+public class VariabelFinal {
+
+    public static void main(String[] args) {
+        final String Nama = "Yulia sagita putri";
+        final int umur = 19;
+        final char kelas = 'A';
+
+        System.out.println("Nama saya = " + Nama);
+        System.out.println("Saya berusia = " + umur);
+        System.out.println("Saya dari kelas informatika = " + kelas + " 2023");
+
+    }
+
+}
