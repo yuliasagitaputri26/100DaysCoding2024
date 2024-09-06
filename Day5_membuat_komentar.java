d1ce1dbd4b226822243f5dffff535802af17d620
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-09-06 02:32:41 -0700

    Day5_membuat_komentar.java

---
+++ Day5_membuat_komentar.java
@@ -0,0 +1,18 @@
+package day5;
+
+public class Komentar {
+
+    public static void main(String[] args) {
+
+        // Data
+        String nama = "Yulia sagita putri";
+        String Nim = "D0223022";
+        char kelas = 'A';
+
+        System.out.println("Nama = " + nama);// digunakan untuk mencetak data dan menambahkan baris baru
+        System.out.printf("Nim %s \n", Nim);// digunakan untuk mencetak teks/data dengan format tertentu 
+        System.out.print("Kelas = " + kelas + "\n");// digunakan untuk mencetak teks dalam bentuk baris
+
+    }
+
+}
