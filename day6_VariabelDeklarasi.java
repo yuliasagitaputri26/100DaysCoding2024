6ef1f75b5b06814ccc6849e98f6612f78cf9cb92
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-09-06 23:38:29 -0700

    day6_VariabelDeklarasi.java

---
+++ day6_VariabelDeklarasi.java
@@ -0,0 +1,17 @@
+package biodata;
+
+public class Deklarasi_variabrel {
+
+    public static void main(String[] args) {
+
+        String Novel = "Cinta Dalam Doa";// Deklarasi VAriabel bertipe Data String
+        int JumlahHalaman = 60;//Deklarasi Vriabel bertipe Data integer;
+        String Penulis = "alyanzyh";
+
+        System.out.println("judul = " + Novel);
+        System.out.println("jumlah Halaman  = " + JumlahHalaman);
+        System.out.println("Penulisnya Yaitu = " + Penulis);
+
+    }
+
+}
