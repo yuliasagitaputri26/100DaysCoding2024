15b98ef27788bfcf6c2fd2280c7f4152860667ac
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-09-08 18:22:20 +0800

    Day7.java

--- Day7.java
+++ Day7.java
@@ -3,10 +3,15 @@ package biodata;
 public class Variabel_Inisialisasi {
 
     public static void main(String[] args) {
-
-        String nama = "Yulia";
-        int jumlah_saudara = 5;
-        boolean dataDiatas = true;
+        //Deklarasi dan inisialisasi String
+        String nama;
+        nama = "kina";
+        
+        int jumlah_saudara;//Deklarasi variabel
+        jumlah_saudara = 4;//Inisialisasi variabel di baris berikutnya
+        
+        boolean dataDiatas;
+        dataDiatas = false;//Inisialisasi terpisah setelah deklarasi
 
         System.out.println("Nama saya : " + nama);
         System.out.println("Saya : " + jumlah_saudara + " Bersaudara");
