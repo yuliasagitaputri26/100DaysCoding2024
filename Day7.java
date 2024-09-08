cc1f12c9e3eadc88dab169e958648b6b2732b24c
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-09-08 00:38:32 -0700

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,17 @@
+package biodata;
+
+public class Variabel_Inisialisasi {
+
+    public static void main(String[] args) {
+
+        String nama = "Yulia";
+        int jumlah_saudara = 5;
+        boolean dataDiatas = true;
+
+        System.out.println("Nama saya : " + nama);
+        System.out.println("Saya : " + jumlah_saudara + " Bersaudara");
+        System.out.println("Ya data diatas : " + dataDiatas);
+
+    }
+
+}
