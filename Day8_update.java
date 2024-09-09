daa5142c761c42f4de26151d12c054b838321d95
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-09-09 02:08:02 -0700

    Day8_update.java

---
+++ Day8_update.java
@@ -0,0 +1,16 @@
+package biodata;
+
+public class update {
+
+    public static void main(String[] args) {
+
+        String Nama = "lala";
+        Nama = "Nana";//Update atau mengubah
+        int nilai = 50;
+        nilai = 90;//update atau mengubah nilai sebelumnya
+
+        System.out.println(Nama);
+        System.out.println(nilai);
+    }
+
+}
