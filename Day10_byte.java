91640582eef0279db93fc997615100af8f6221e4
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-09-11 02:52:49 -0700

    Day10_byte.java

---
+++ Day10_byte.java
@@ -0,0 +1,17 @@
+package Day10;
+
+public class TipeDatabyte {
+
+    public static void main(String[] args) {
+        // Tipe data byte Nilai Min = -128 dan nilai Max = 127
+        byte a = 17;
+        byte b = 20;
+        byte c = 100;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+
+}
