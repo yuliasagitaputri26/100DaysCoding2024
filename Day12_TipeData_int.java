c6c4c16807d16938defb5d850813ee7f1b954b4b
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-09-13 03:15:03 -0700

    Day12_TipeData_int.java

---
+++ Day12_TipeData_int.java
@@ -0,0 +1,29 @@
+package Day10;
+
+public class Hari12 {
+
+    public static void main(String[] args) {
+        //Inisialisasi
+        int a = 4;
+        int b = 6;
+        int c = 10;
+        int d = 20;
+        //Mencetak nilai menggunakan printf
+        System.out.printf("Nilai dari A adalah %d \n", a);
+        System.out.printf("Nilai dari B adalah %d \n", b);
+        System.out.printf("Nilai dari C adalah %d \n", c);
+        System.out.printf("Nilai dari D adalah %d \n", d);
+
+        //Update nilai
+        a = 100;
+        b = 40;
+        c = 60;
+
+        //Mencetak nilai yang telah diUpdate
+        System.out.println("Nilai A setelah di update = " + a);
+        System.out.println("Nilai B setelah di update = " + b);
+        System.out.println("Nilai C setelah di update = " + c);
+
+    }
+
+}
