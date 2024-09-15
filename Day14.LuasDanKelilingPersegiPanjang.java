ba4194eef38e1f9cc51c5a0d6020f334952a15fd
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-09-15 02:11:25 -0700

    Day14.LuasDanKelilingPersegiPanjang.java

---
+++ Day14.LuasDanKelilingPersegiPanjang.java
@@ -0,0 +1,19 @@
+package Day10;
+
+public class Aritmatika {
+
+    public static void main(String[] args) {
+        //Inisialisasi nilai dari variabel panjang dan lebar
+        int Panjang = 10;
+        int Lebar = 6;
+
+        //Mencari luas persegi panjang
+        int luas = Panjang * Lebar;
+        int Keliling = 2 * (Panjang + Lebar);
+
+        //Menampilkan hasil luas dan keliling dari persegi panjang
+        System.out.printf("Luas dari persegi panjang adalah %d \n ", luas);
+        System.out.printf("Keliling dari persegi panjang adalah %d \n", Keliling);
+    }
+
+}
