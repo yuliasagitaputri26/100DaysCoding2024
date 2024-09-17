ca568130a5afe215b96f8d3479d99072ff5a1b8e
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-09-17 05:19:48 -0700

    Day16.java

---
+++ Day16.java
@@ -0,0 +1,27 @@
+package Day10;
+
+public class Hari16 {
+
+    public static void main(String[] args) {
+
+        //Operasi Aritmatika (penjumlahan,pengurangan,Perkalian, pembagian dan modulus)
+        
+        //variabel a dan b diinisialisasi dengan nilai 10 dan 2
+        int a = 10;
+        int b = 2;
+        
+        //Melakukan beberapa operasi
+        int c = a + b;
+        int d = a - b;
+        int e = a * b;
+        int f = a / b;
+        int g = a % b;
+        //Mencetak untuk menampilkan hasil dari operasi di atas
+        System.out.println("Hasil penjumlahan = " + c);
+        System.out.println("Hasil pengurangan = " + d);
+        System.out.println("Hasil perkalian = " + e);
+        System.out.println("Hasil pembagian = " + f);
+        System.out.println("Hasil sisa bagi = " + g);
+    }
+
+}
