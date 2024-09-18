5a2f94a96f0611731789b4fe1c52b6b5ab951154
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-09-18 03:52:43 -0700

    Day17.operatorPenugasan.java

---
+++ Day17.operatorPenugasan.java
@@ -0,0 +1,25 @@
+package Day10;
+
+public class Hari17 {
+
+    public static void main(String[] args) {
+
+        //Operator penugasan : pengisisan dan perkalian , pengisian dan pembagian
+        //Deklarasi dan Inisialisasi variabel nilai1 dan nilai2
+        int nilai1 = 20;
+        int nilai2 = 40;
+        //mencetak nilai dari varibel nilai1 dan nilai2 
+        System.out.println(nilai1);
+        System.out.println(nilai2);
+
+        //operator penugasan dengan cara mengisi dan mengalikan
+        nilai1 *= 2;
+        System.out.println("Nilai setelah dikalikan : " + nilai1);
+
+        //Mengisi dan membagi
+        nilai2 /= 4;
+        System.out.println("Nilai setelah dibagi : " + nilai2);
+
+    }
+
+}
