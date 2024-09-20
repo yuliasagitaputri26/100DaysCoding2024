65501e0c45347c8e15b0c16556680020b79168ef
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-09-20 03:18:46 -0700

    Hari19.java

---
+++ Hari19.java
@@ -0,0 +1,15 @@
+package Day10;
+
+public class Hari19 {
+
+    public static void main(String[] args) {
+        //Operator Penugasan = pengisian dan modulu
+
+        byte a = 30;
+        System.out.println(a);
+
+        a %= 7;
+        System.out.println("Modulu dari a adalah = " + a);
+    }
+
+}
