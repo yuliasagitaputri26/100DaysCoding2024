13da0daa756aa7f9fa0ec556ccf362e0f9a7366d
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-09-21 03:55:23 -0700

    Day20_InputDariKeyword.java

---
+++ Day20_InputDariKeyword.java
@@ -0,0 +1,20 @@
+package Day10;
+
+import java.util.Scanner;
+
+public class Hari20 {
+
+    public static void main(String[] args) {
+        // Input dari keyboard
+
+        Scanner yulia = new Scanner(System.in);
+
+        System.out.print("Masukkan nama anda : ");
+        String nama = yulia.nextLine();
+
+        System.out.print("Masukkan umur anda : ");
+        int umur = yulia.nextInt();
+
+    }
+
+}
