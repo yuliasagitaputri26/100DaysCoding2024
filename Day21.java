a6e762ff7e4a020c3a7cbd213e1c623543a84c75
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-09-22 05:13:53 -0700

    Day21.java

---
+++ Day21.java
@@ -0,0 +1,17 @@
+package Day10;
+
+//mendeklarasi sebuah kelas pablik bernama hari19
+public class Hari21 {
+
+    //mendefinisikan metode main,yang merupakan titik awal eksekusi program
+    public static void main(String[] args) {
+        //operator perbandingan 
+        long panjang1 = 10l;
+        long panjang2 = 30l;
+
+        System.out.println("panjang dari variabel panjang satu : " + (panjang1 < panjang2));
+        System.out.println("panjang dari variabel panjang dua : " + (panjang2 > panjang1));
+        System.out.println("panjang dari variabel panjang dua : " + (panjang2 < panjang1));
+
+    }
+}
