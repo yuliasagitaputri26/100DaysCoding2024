edd97681bb347df41960a71f73f85717550d47a5
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-09-23 04:38:40 -0700

    Day22.java

---
+++ Day22.java
@@ -0,0 +1,16 @@
+package Day10;
+
+public class Hari22 {
+
+    public static void main(String[] args) {
+
+        //operator penugasan : pengisian nilai,pengisian dan penambahan,pengisian dan pengurangan
+        int nilai = 10;//pengisian nilai 
+        nilai += 30;//pengisian dan penambahan
+        System.out.println("Hasil penambahanya adalah : " + nilai);
+        nilai -= 20;//pengisian dan pengurangan
+        System.out.println("hasil pengurangan : " + nilai);
+
+    }
+
+}
