d4f4f2eb9fefba196ece2e2148591565eeba50d5
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-09-25 06:37:49 -0700

    Day24.java

---
+++ Day24.java
@@ -0,0 +1,16 @@
+package Day10;
+
+public class Hari24 {
+    
+    public static void main(String[] args) {
+        //konversi String ke tipedata Primitif
+        
+        String a = "12";
+        int value = Integer.parseInt(a);
+        
+        System.out.println("String yang telah diubah :" + value);
+
+      
+    }
+
+}
