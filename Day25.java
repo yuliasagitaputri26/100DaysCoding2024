958bf4a006fc03cdb81281c983bb58acadee1b18
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-09-26 05:01:16 -0700

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,26 @@
+package Day10;
+
+import java.util.Scanner;
+
+public class Hari25 {
+
+    public static void main(String[] args) {
+
+        Scanner a = new Scanner(System.in);
+
+        System.out.println("Masukkan angka yang ingin dikalikan: ");
+        int angka = a.nextInt();
+
+        System.out.println(angka + " x 1 = " + (angka * 1));
+        System.out.println(angka + " x 2 = " + (angka * 2));
+        System.out.println(angka + " x 3 = " + (angka * 3));
+        System.out.println(angka + " x 4 = " + (angka * 4));
+        System.out.println(angka + " x 5 = " + (angka * 5));
+        System.out.println(angka + " x 6 = " + (angka * 6));
+        System.out.println(angka + " x 7 = " + (angka * 7));
+        System.out.println(angka + " x 8 = " + (angka * 8));
+        System.out.println(angka + " x 9 = " + (angka * 9));
+        System.out.println(angka + " x 10 = " + (angka * 10));
+    }
+
+}
