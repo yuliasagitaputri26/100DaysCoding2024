01d2300d7058d94a775503a4fb9d2df94d4d0096
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-09-27 05:37:50 -0700

    Day26.java

---
+++ Day26.java
@@ -0,0 +1,19 @@
+package latihan;
+
+public class Mahasiswa {
+ 
+    public static void main(String[] args) {
+
+        //Konversi tipe data int ke String
+        int a = 80;
+        String b = String.valueOf(a);
+        System.out.println("Nilai yang dikonversi : " + b);
+        
+        //konversi tipe data float ke String
+        float c = 80.0f;
+        String d = String.valueOf(c);
+        System.out.println("Nilai yang dikonversi : " + d);
+
+    }
+
+}
