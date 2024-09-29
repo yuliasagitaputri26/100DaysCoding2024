fb94e4737e876dd93c14d89b645e78c1ecd32541
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-09-29 04:50:05 -0700

    Day28.java

---
+++ Day28.java
@@ -0,0 +1,17 @@
+package Day10;
+
+public class Hari28 {
+   
+    public static void main(String[] args) {
+        
+        //konversi String ke byte
+        String nilai = "100";
+        byte nilai1 = Byte.parseByte(nilai);
+        System.out.println("Nilai yang telah di konversi adalah : " + nilai1);
+        //konversi String ke short
+        String nilai2 = "101";
+        short nilai3 = Short.parseShort(nilai2);
+        System.out.println("Nilai yang telah di konversi adalah : " + nilai3);
+    }
+
+}
