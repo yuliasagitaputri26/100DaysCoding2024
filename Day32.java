9aab9152b06cb8e370707e5a4320e7dcb7016409
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-10-03 04:02:27 -0700

    Day32.java

---
+++ Day32.java
@@ -0,0 +1,12 @@
+package Day10;
+
+public class Hari32 {
+
+    public static void main(String[] args) {
+
+        //Type Casting dari tipedata double ke tipedata int
+        double nilai = 60.5;
+        int nilaiInt = (int) nilai;
+
+        System.out.println("Nilai double: " + nilai);
+        System.out.println("Nilai setelah casting ke tipedata int: " + nilaiInt);
