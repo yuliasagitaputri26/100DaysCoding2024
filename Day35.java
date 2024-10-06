94afb61761a9d60b14a8201f03455d8c91829eb4
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-10-06 03:33:41 -0700

    Day35.java

---
+++ Day35.java
@@ -0,0 +1,18 @@
+package Day10;
+
+public class Hari35 {
+
+    public static void main(String[] args) {
+        //operator aritmatika penjumlahan dan pengurangan
+        
+        byte nilaiPertama = 10;
+        byte nilaiKedua = 20;
+        
+        int Hasil =  nilaiPertama + nilaiKedua;
+        System.out.println("Hasil Penjumlahan: " + Hasil);
+        
+        int Hasilpengurangan = nilaiKedua - nilaiPertama;
+        System.out.println("Hasil pengurangan: " + Hasilpengurangan);
+    }
+
+}
