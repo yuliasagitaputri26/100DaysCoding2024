1807b546ed1a482520f4fb2682c40da2092cf74f
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-10-08 04:35:53 -0700

    Day37.java

---
+++ Day37.java
@@ -0,0 +1,18 @@
+package Day10;
+
+public class Hari37 {
+
+    public static void main(String[] args) {
+        //operator penugasan: pengisian nilai, pengisian dan penjumlahan,pengisian dan pengurangan
+
+        int a = 20; //pengisian nilai
+        System.out.println(a);
+
+        a += 10;//pengisian dan penambahan
+        System.out.println("Nilai setelah penambahan: " + a);
+
+        a -= 5;//pengisian dan pengurangan
+        System.out.println("Nilai setelah pengurangan: " + a);
+    }
+
+}
