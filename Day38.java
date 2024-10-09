f56d97f6aec4caded9e9937643da65eac9689672
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-10-09 03:38:24 -0700

    Day38.java

---
+++ Day38.java
@@ -0,0 +1,17 @@
+package Day10;
+
+public class Hari38 {
+
+    public static void main(String[] args) {
+        //Operator penugasan: pengisian dan perkalian, pengisian dan pembagian
+        int angka1 = 10;
+        angka1 *= 10;
+        System.out.println("Hasil perkalian: " + angka1);
+
+        int angka2 = 50;
+        angka2 /= 2;
+        System.out.println("Hasil pembagian: " + angka2);
+
+    }
+
+}
