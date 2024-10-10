0f75de9d18805e6150d3a4f4eae5627ed08d4dc7
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-10-10 03:43:45 -0700

    Day39.java

---
+++ Day39.java
@@ -0,0 +1,16 @@
+package Day10;
+
+public class Hari39 {
+    
+    public static void main(String[] args) {
+        //operator penugasan: pengisian dan modulu
+       
+        int nilai;
+        nilai = 25;
+        nilai *= 2;
+        System.out.println("Nilai hasil kali 2: " + nilai);
+        nilai %= 3;
+        System.out.println("Nilai dari sisa bagi adalah: " + nilai);
+    }
+
+}
