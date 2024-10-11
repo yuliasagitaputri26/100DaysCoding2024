47d1fcd4acf6c20910816d0fa26ae1f8ba5cb4de
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-10-11 03:40:48 -0700

    Day40.java

---
+++ Day40.java
@@ -0,0 +1,22 @@
+package Day10;
+
+public class Hari40 {
+
+    public static void main(String[] args) {
+        //Operator perbandingan: Lebih besar dan lebih kecil
+        int nilaiPertama = 120;
+        int nilaiKedua = 200;
+
+        int hasil = nilaiPertama - nilaiKedua;
+        int hasil1 = nilaiKedua * nilaiPertama;
+        System.out.println(hasil);
+        System.out.println(hasil1);
+
+        System.out.println(hasil < hasil1);
+        System.out.println(hasil1 < hasil);
+        System.out.println(hasil1> hasil);
+        System.out.println(hasil > hasil1);
+
+    }
+
+}
