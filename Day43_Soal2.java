d71bc05dc21e57da611a62b1024aec2be59e0a25
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-10-14 03:09:42 -0700

    Day43_Soal2.java

---
+++ Day43_Soal2.java
@@ -0,0 +1,23 @@
+package Day10;
+
+public class Hari43Soal2 {
+
+    public static void main(String[] args) {
+        
+        int a = 15;
+        int b = 20;
+        
+        System.out.println(b);
+        b %= a;
+        System.out.println(b);
+        b *= a;
+        System.out.println(b);
+        b -= a;
+        System.out.println(b);
+        b /= a;
+        System.out.println(b);
+        b += a;
+        System.out.println(b);
+    }
+
+}
