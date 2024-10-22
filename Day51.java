aaa38686654aa7c6b4ab89989f4bcc410027269b
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-10-22 05:18:27 -0700

    Day51.java

---
+++ Day51.java
@@ -0,0 +1,35 @@
+package Day10;
+
+public class Hari51 {
+
+    public static void main(String[] args) {
+        //percabangan switch case
+        
+        int hari = 2;
+        switch (hari) {
+
+            case 1:
+                System.out.println("Senin");
+                break;
+            case 2:
+                System.out.println("Selasa");
+                break;
+            case 3:
+                System.out.println("Rabu");
+                break;
+            case 4:
+                System.out.println("Kamis");
+                break;
+            case 5:
+                System.out.println("Jum'at");
+                break;
+            case 6:
+                System.out.println("Sabtu");
+                break;
+            case 7:
+                System.out.println("Minggu");
+                break;
+        }
+    }
+
+}
