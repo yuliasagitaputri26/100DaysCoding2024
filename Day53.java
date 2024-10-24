baa51d61927147f69302c6f1e96ebf3c7578f3f6
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-10-24 04:32:04 -0700

    Day53.java

---
+++ Day53.java
@@ -0,0 +1,21 @@
+package Day10;
+
+import java.util.Scanner;
+
+public class Hari53 {
+
+    public static void main(String[] args) {
+        //looping: for
+
+        Scanner yulia = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        int angka = yulia.nextInt();
+
+        //
+        for (int i = 1; i <= angka; i++) {
+            System.out.println(i);
+
+        }
+    }
+
+}
