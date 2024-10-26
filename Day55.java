6d266db5485ef4e7b84e56e866de77578ff552b0
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-10-26 04:55:09 -0700

    Day55.java

---
+++ Day55.java
@@ -0,0 +1,17 @@
+package Day10;
+
+import java.util.Scanner;
+
+public class Hari55 {
+
+    public static void main(String[] args) {
+        //Looping for decrerment
+        Scanner yu = new Scanner(System.in);
+        System.out.print("Masukkan nilai awal: ");
+        int nilai = yu.nextInt();
+        for (int i = 10; i > 0; i--) {
+            System.out.println("Nilai i: " + i);
+        }
+    }
+
+}
