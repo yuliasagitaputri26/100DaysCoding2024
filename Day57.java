a8b273c8a8d5ed13d88882c7797a84d5e3104ce5
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-10-28 03:37:59 -0700

    Day57.java

---
+++ Day57.java
@@ -0,0 +1,16 @@
+package Day10;
+
+public class Hari57 {
+   
+    public static void main(String[] args) {
+        //Looping : while increment
+        int angka = 2;
+        
+        while(angka <= 20){
+            System.out.println(angka);
+            angka +=2;
+        }
+ 
+    }
+
+}
