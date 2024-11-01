ef6e38e80912ac5bb9bf85e2064a28abae24387a
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-11-01 19:32:01 +0800

    Day61.java

---
+++ Day61.java
@@ -0,0 +1,15 @@
+package Day10;
+
+public class Hari61 {
+
+    public static void main(String[] args) {
+
+        //Infinite Loop
+        int a = 1;
+        while (true) {
+            System.out.println("Loop  ke- : " + a + " akan berjalan trus");
+            a += 2;
+        }
+
+    }
+}
