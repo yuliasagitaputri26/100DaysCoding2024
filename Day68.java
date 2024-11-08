5c9253c7cf3d7c1f2e91f18df5a89be82f2dbd62
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-11-08 19:50:52 +0800

    Day68.java

---
+++ Day68.java
@@ -0,0 +1,15 @@
+package Day10;
+
+public class Hari68 {
+
+    //Method : Non-void
+    public int hitungJumlah() {
+        return 5;
+    }
+    
+    public static void main(String[] args) {
+        Hari68 Nilai = new Hari68();
+        int angka = Nilai.hitungJumlah(); 
+        System.out.println("Angka yang dikembalikan: " + angka);
+    }
+}
