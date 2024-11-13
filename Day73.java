64d7ea0b5b190fe620473d5873794cf7248a4ab5
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-11-13 18:59:17 +0800

    DayStringMethodcharAt73.java

---
+++ Day73.java
@@ -0,0 +1,21 @@
+package Day10;
+
+public class Hari73 {
+
+    public static void main(String[] args) {
+        //String method : charAt()
+        String nama = "Yulia";
+
+        char a = nama.charAt(0);
+        char b = nama.charAt(3);
+        char c = nama.charAt(4);
+        
+        System.out.println(nama);
+        System.out.println("----------------Karakter yang dipanggil----------------");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+
+}
