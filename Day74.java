81b91803bd55781f2f6cef5729c9a9f49a4f48cc
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-11-14 17:29:22 +0800

    Day74StringMethodLength.java

---
+++ Day74.java
@@ -0,0 +1,18 @@
+package Day10;
+
+public class Hari74 {
+
+    public static void main(String[] args) {
+
+        //String method : length
+        String namaBunga = "Mawar";
+
+        char a = namaBunga.charAt(0);
+        System.out.println(a);
+        
+        int panjang = namaBunga.length();
+        System.out.println(panjang + " karakter");
+
+    }
+
+}
