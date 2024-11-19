f87ac6ed57bf76df43ae9a6ef16051464dee47a4
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-11-19 20:09:43 +0800

    Day79.java

---
+++ Day79.java
@@ -0,0 +1,17 @@
+package Day10;
+
+import java.util.Scanner;
+
+public class Hari79 {
+
+    public static void main(String[] args) {
+        
+        Scanner yulia = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        int a = yulia.nextInt();
+        
+        String b = Integer.toString(a);
+        System.out.println(b);
+        
+    }
+}
