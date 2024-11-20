6e9b6e0b5294b0a5dd437169cc1c178dea56ef9b
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-11-20 20:06:00 +0800

    Day80.java

---
+++ Day80.java
@@ -0,0 +1,14 @@
+package Day10;
+
+public class Hari80 {
+
+    public static void main(String[] args) {
+
+        String nama = " Hai, Yulia  ";
+        String a = nama.trim();
+
+        System.out.println("Before '" + nama + "'");
+        System.out.println("After '" + a + "'");
+    }
+
+}
