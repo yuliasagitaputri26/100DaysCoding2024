2e063f34dc39819eab97c18e1c3904657572465b
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-11-22 18:43:29 +0800

    Day82.java

---
+++ Day82.java
@@ -0,0 +1,18 @@
+package Day10;
+
+public class Hari82 {
+
+    public static void main(String[] args) {
+        //Method ceil,floor,dan round
+ 
+        double nilai = 6.3;
+
+        System.out.println("Nilai awal: " + nilai);
+
+        System.out.println("Nilai ceil (dibulatkan ke atas): " + Math.ceil(nilai));//math.ceil untuk membulatkan keatas
+        System.out.println("Nilai floor (dibulatkan ke bawah): " + Math.floor(nilai));//math.floor untuk membulatkan kebawah
+        System.out.println("Nilai round (dibulatkan ke angka terdekat): " + Math.round(nilai));//math.roound untuk membulatkan angka yang terdekat
+
+    }
+
+}
