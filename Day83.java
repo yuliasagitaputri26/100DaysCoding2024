26936a671eeb56057b42214884978e519c6ec8a8
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-11-23 21:05:54 +0800

    Day83.java

---
+++ Day83.java
@@ -0,0 +1,14 @@
+package Day10;
+
+public class Hari83 {
+
+    public static void main(String[] args) {
+        //Membuat objek Array satu dimensi
+
+        // Deklarasi dan inisialisasi array
+        int[] a = new int[5];
+        int[] b = new int[5];
+
+    }
+
+}
