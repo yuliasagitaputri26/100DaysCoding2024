3edec6bb1a5ec7a183ae5fcde1a72c4e92d376a8
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2024-11-25 20:14:31 +0800

    Day85.java

---
+++ Day85.java
@@ -0,0 +1,12 @@
+package Day10;
+
+public class Hari85 {
+
+    public static void main(String[] args) {
+
+        //Mengisi elemen ke Array 1 dimensi pada saat di Inisialisasi
+        String hewan[] = {"Kucing", "Ayam", "Sapi", "Kelinci", "Gajah"};
+        String bunga[] = {"Mawar" , "Melati" , "Matahari" , "Anggrek" , "Tulip"};
+
+    }
+}
