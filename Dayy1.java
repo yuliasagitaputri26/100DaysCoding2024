7e52623468ac70dfa363a94af3399a8a8ddc0d87
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2025-09-08 11:26:07 +0800

    Create Dayy1.java

---
+++ Dayy1.java
@@ -0,0 +1,35 @@
+package biodata;
+
+public class Biodata {
+
+    public static void main(String[] args) {
+        
+        // Membuat Biodata atau Data Diri
+
+        String Nama = "Yulia Sagita Putri";
+        String TTL = " Papalang, 06 Juli 2005";
+        char JenisKelamin = 'P';
+        int Umur = 19;
+        String Alamat = "Papalang";
+        String Alumni = "SMKN 1 PAPALANG";
+        String Kampus = "Universitas Sulawesi Barat";
+        String Fakultas = "Teknik";
+        String Prodi = "Informatika";
+        String Nim = "D0223022";
+        Boolean DataDiri = true;
+
+        System.out.println("Nama = " + Nama);
+        System.out.println("TTL = " + TTL);
+        System.out.println("JenisKelamin = " + JenisKelamin);
+        System.out.println("Umur = " + Umur);
+        System.out.println("Alamat = " + Alamat);
+        System.out.println("Alumni = " + Alumni);
+        System.out.println("kampus = " + Kampus);
+        System.out.println("Fakultas = " + Fakultas);
+        System.out.println("Prodi = " + Prodi);
+        System.out.println("Nim = " + Nim);
+        System.out.println("DataDiri = " + DataDiri);
+
+    }
+
+}
